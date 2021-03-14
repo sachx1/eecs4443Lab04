2b0d155f24e2b5ca00a4084281e3d35786abdfa1
Sachin Khargie <sachx1281@gmail.com>
2021-03-14 17:18:23 -0400

    lab 4 files

---
+++ app/src/main/java/ca/yorku/eecs/mack/demotiltball70505/ending.java
@@ -0,0 +1,13 @@
+package ca.yorku.eecs.mack.demotiltball70505;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ending extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup);
+    }
+}
